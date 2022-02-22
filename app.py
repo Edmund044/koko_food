c189cb7cc290df28635f18a80637db807b46fbb2
Edmund Opiyo <edmundopiyo@gmail.com>
2022-02-22 12:39:24 +0300

    flask deployment 1

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/hi")
+def hello():
+    return render_template("index.html")
+
+@app.route("/sub", methods=['GET', 'POST'])
+def submit():
+    if request.method == "POST":
+        name = request.form["username"]
+
+        return render_template("sub.html", n = name)
+       
+
+
+if __name__ == "__main__":
+    app.run(debug=True)    
