3292179e80081e086458b5b5906b0c57b3d6151b
Edmund Opiyo <edmundopiyo@gmail.com>
2022-03-11 13:49:20 +0300

    added requirements 2

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+from random import randint
 
-
-@app.route("/hi")
+@app.route("/input")
 def hello():
     return render_template("index.html")
 
-@app.route("/sub", methods=['GET', 'POST'])
+@app.route("/prediction", methods=['GET', 'POST'])
 def submit():
     if request.method == "POST":
-        name = request.form["username"]
-
-        return render_template("sub.html", n = name)
+        income = int(request.form["income"])
+        limit =  randint(0,100)/100 * income
+        # if income > 1000 and income <= 5000:
+        #     limit = 1000
+        # elif income > 3000 and income <= 50000:
+        #     limit = 2000  
+        # else:
+        #     limit = 0     
+        return render_template("prediction.html", n = limit)
        
 
 
