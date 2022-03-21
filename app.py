d384a52524a1e4ab4a3122bda6e1d982e1fa242f
EdmundEngineer <97869278+EdmundEngineer@users.noreply.github.com>
2022-03-21 16:11:55 +0300

    Update app.py
    fgreg

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello():
 def submit():
     if request.method == "POST":
         income = int(request.form["income"])
-        limit =  randint(0,100)/100 * income
+        limit =  randint(0,20)/100 * income
         # if income > 1000 and income <= 5000:
         #     limit = 1000
         # elif income > 3000 and income <= 50000:
